39dd27c81c3ea3a04754668ba168148f502d35e2
khan2626 <israelkhan217@gmail.com>
2023-03-23 15:31:02 -0700

    4-print_most_numbers.c_updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	int ch
+	int ch;
 
-	for (ch = 0; ch < 10; ch++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch != 2 && ch != 4)
+		if (ch != 50 && ch != 52)
 			_putchar(ch);
 	}
 	_putchar('\n');
