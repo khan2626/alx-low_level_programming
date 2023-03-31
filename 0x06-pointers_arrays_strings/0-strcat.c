d451a12c3ab8168d41e6a59654789cdad1c4bc70
khan2626 <israelkhan217@gmail.com>
2023-03-31 00:07:21 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 	int lendest = 0;
 	int lensrc = 0;
-	int i = 0;
+	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		lendest++;
 	for (i = 0; src[i] != '\0'; i++)
 		lensrc++;
 	for (i = 0; i <= lensrc; i++)
-		dest[lendest + 1] = src[i];
+		dest[lendest + i] = src[i];
 	return (dest);
 }
