5f5a7ef97ec1c6f01e03cc14b610e86d31f4aee7
khan2626 <israelkhan217@gmail.com>
2023-04-05 09:07:16 -0700

    0-puts_recursion.c_updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
-		_putchar(s[i]);
+	if (*s != '\0')
+		_putchar(*s);
+	_puts_recursion(s + 1);
 	else
 		_putchar('\n');
 }
