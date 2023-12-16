1a219bcaf83ca31e2ab5fa29c2dee8e94b0cf07e
khan2626 <israelkhan217@gmail.com>
2023-12-16 09:52:07 +0100

    time_complexity

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,17 +11,17 @@ int linear_search(int *array, size_t size, int value)
 {
   size_t i;
 
-  if (array == NULL)
+  if (!array)
     {
       return (-1);
     }
-  for (i = 0; i < size; i++)
+  i = 0;
+  while (i < size)
     {
-      printf("Value checked array [%li] = [%i]\n", i, array[i]);
+      printf("Value checked array [%lu] = [%d]\n", i, array[i]);
       if (array[i] == value)
-	{
-	  return (i);
-	}
+	return (i);
+      i++;
     }
   return (-1);
 }
